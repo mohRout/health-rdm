e205c1934bd46def604235fb0ccbea36d08fcc74
mohRout <mohanrushan@gmail.com>
2024-11-08 22:51:02 +0530

    added Navigation bar to health rdm

---
+++ src/Pages/Landingpage.js
@@ -0,0 +1,13 @@
+import { Box, Toolbar } from '@mui/material'
+import React from 'react'
+import Navbar from '../Dashboard/Navbar'
+
+const Landingpage = () => {
+  return (
+    <Box>
+       <Navbar/> 
+    </Box>
+  )
+}
+
+export default Landingpage
